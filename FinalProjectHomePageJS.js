6acebadd54b5226b4154f53287a7a4a11f98430b
rs17rw <43220367+rs17rw@users.noreply.github.com>
2018-11-30 22:25:43 -0500

    Add files via upload

---
+++ FinalProjectHomePageJS.js
@@ -0,0 +1,4 @@
+var myConfirm = confirm("This page is all about Robert Sharp");
+console.log(myConfirm);
+
+document.getElementById("para").innerHTML=Date();
---
+++ FinalProjectMusicPageJS.js
@@ -0,0 +1,4 @@
+document.getElementById("para").innerHTML=Date();
+
+var myConfirm = confirm("This page is all about Music");
+console.log(myConfirm);
